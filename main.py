e5547a991eda2277aaa2c23d34ae923e5ba437fe
SergiodelaMata <sergio.matam@uah.es>
2024-03-13 19:40:50 +0100

    :tada::sparkles: Initialized the first process to make the combination of the results.

---
+++ main.py
@@ -0,0 +1,25 @@
+import filesCSV as fCSV
+import filesExcel as fExcel
+import treatment_data as tData
+
+
+def main():
+    equip_ids = [70, 89, 101, 134, 137, 143]
+    # equip_ids = [134, 143]
+    algorithms = ['linear_regression', 'ridge', 'lasso', 'elastic_net', 'decision_tree', 'random_forest', 'svr',
+                    'knn', 'polynomial_regression', 'logistic_regression', 'naive_bayes', 'gaussian_process']
+    # algorithms = ['decision_tree', 'knn']
+    situations = ['DAYOFWEEK_HOUR', 'DAYOFMONTH_HOUR', 'WEEKEND_HOUR', 'HOLIDAY_HOUR']
+
+    tData.combination_treatment_considering_only_equipments(equip_ids, algorithms, [0, 1, 2, 3])
+
+
+    """for equip_id in equip_ids:
+        writer = None
+        for counter in range (0, 4):
+            dataframe = fCSV.unify_data_equip_algorithms_situation(equip_id, algorithms, counter)
+            fCSV.save_compendium_results_equips_situation(equip_id, counter, dataframe)
+            writer = fExcel.save_compendium_results_equips_situation(equip_id, counter, dataframe, writer)"""
+
+
+main()
