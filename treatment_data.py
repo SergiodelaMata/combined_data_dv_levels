11a86975b2d9a8c0991abbae35e3a5193883a5b0
SergiodelaMata <sergio.matam@uah.es>
2024-03-13 20:09:37 +0100

    :sparkles: Created the methods to get all the results from each combination between equipment and algorithm, make their average and save those results in CSV files for each combination.

--- treatment_data.py
+++ treatment_data.py
@@ -132,6 +132,47 @@ def get_all_data_from_situation(list_equip_ids, list_algorithms, situation):
     return dataframe
 
 
+def combination_treatment_considering_equipments_algorithms(equip_ids, algorithms, situations):
+    """
+    list, list, list --> None
+    OBJ: EN: Make the combination of the results predictions considering the equipments and the algorithms and save the
+    results in CSV files.
+    ES: Hacer la combinación de los resultados de las predicciones considerando los equipos y los algoritmos y guardar
+    los resultados en archivos CSV.
+    :param equip_ids: EN: List of the IDs of the equipments. ES: Lista de los IDs de los equipos.
+    :param algorithms: EN: List of the algorithms used to make the predictions. ES: Lista de los algoritmos usados
+    para hacer las predicciones.
+    :param situations: EN: List of the situations used to make the predictions. ES: Lista de las situaciones usadas
+    para hacer las predicciones.
+    :return: None
+    """
+    for equip_id in equip_ids:
+        for algorithm in algorithms:
+            dataframe = get_all_data_from_equipment_algorithm(equip_id, algorithm, situations)
+            dataframe_aux = average_results_columns_dataframe(dataframe)
+            fCSV.save_combination_results_two_categories(1, equip_id, algorithm, dataframe_aux)
+
+
+def get_all_data_from_equipment_algorithm(equip_id, algorithm, list_situations):
+    """
+    int, string, list --> Dataframe
+    OBJ: EN: Get all the data from an equipment with different algorithms and situations.
+    :param equip_id: EN: Equipment ID. ES: ID del equipo.
+    :param algorithm: EN: Algorithm used to make the predictions. ES: Algoritmo usado para hacer las predicciones.
+    :param list_situations: EN: List of the situations used to make the predictions. ES: Lista de las situaciones usadas
+    para hacer las predicciones.
+    :return: EN: Dataframe with the data from the equipment. ES: Dataframe con los datos del equipo.
+    """
+    dataframe = pd.DataFrame()
+    for situation in list_situations:
+        dataframe_temp = fCSV.get_data_equip_algorithm_analysis(equip_id, algorithm, situation)
+        dataframe = pd.concat([dataframe, dataframe_temp], axis=0)
+    # EN: Update the index of the dataframe.
+    # ES: Actualizar el índice del dataframe.
+    dataframe = dataframe.reset_index(drop=True)
+    return dataframe
+
+
 def average_results_columns_dataframe(dataframe):
     """
     Dataframe --> Dataframe
